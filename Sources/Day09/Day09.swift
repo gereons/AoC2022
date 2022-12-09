48c159d76ed86f0845666082de1b76a8f21ca8ba
Gereon Steffens <gereon@steffens.org>
2022-12-09 09:30:18 +0100

    day 9 part 1

--- Sources/Day09/Day09.swift
+++ Sources/Day09/Day09.swift
@@ -6,17 +6,88 @@
 
 import AoCTools
 
+private struct Motion {
+    let dir: Point.Direction
+    let dist: Int
+
+    init(_ str: String) {
+        let parts = str.components(separatedBy: " ")
+        dist = Int(parts[1])!
+        switch parts[0] {
+        case "U": dir = .n
+        case "L": dir = .w
+        case "R": dir = .e
+        case "D": dir = .s
+        default: fatalError()
+        }
+    }
+}
+
 final class Day09: AOCDay {
+    private let motions: [Motion]
 
     init(rawInput: String? = nil) {
-        // let input = rawInput ?? Self.rawInput
+        let input = rawInput ?? Self.rawInput
+        motions = input.lines.map { Motion($0) }
     }
 
     func part1() -> Int {
-        return 0
+        var head = Point.zero
+        var tail = head
+        var visited = Set<Point>()
+
+        for motion in motions {
+            for _ in 0 ..< motion.dist {
+                head = head.moved(motion.dir)
+                let dx = head.x - tail.x
+                let dy = head.y - tail.y
+                if abs(dx) > 1 || abs(dy) > 1 {
+                    // move tail
+                    if dx == 0 || dy == 0 {
+                        tail = tail.moved(motion.dir)
+                    } else {
+                        if abs(dx) == 2 {
+                            tail = Point(head.x - dx.signum(), head.y)
+                        } else if abs(dy) == 2 {
+                            tail = Point(head.x, head.y - dy.signum())
+                        } else {
+                            fatalError()
+                        }
+                    }
+                }
+                visited.insert(tail)
+            }
+        }
+
+        return visited.count
     }
 
     func part2() -> Int {
         return 0
+    }
+
+    private func draw(head: Point, tail: Point, visited: Set<Point>) {
+        let minX = min(head.x, tail.x, visited.map { $0.x }.min()! )
+        let minY = min(head.y, tail.y, visited.map { $0.y }.min()! )
+        let maxX = max(head.x, tail.x, visited.map { $0.x }.max()! )
+        let maxY = max(head.y, tail.y, visited.map { $0.y }.max()! )
+
+        for y in minY...maxY {
+            for x in minX...maxX {
+                let p = Point(x, y)
+                let ch: Character
+                if p == head {
+                    ch = "H"
+                } else if p == tail {
+                    ch = "T"
+                } else if p == .zero {
+                    ch = "s"
+                } else {
+                    ch = "."
+                }
+                print(ch, terminator: "")
+            }
+            print()
+        }
     }
 }
--- Tests/Day09Tests.swift
+++ Tests/Day09Tests.swift
@@ -4,8 +4,16 @@ import XCTest
 final class Day09Tests: XCTestCase {
     func testDay09_1() throws {
         let day = Day09(rawInput: """
+R 4
+U 4
+L 3
+D 1
+R 4
+D 1
+L 5
+R 2
 """)
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 13)
     }
 
     func testDay09_2() throws {
