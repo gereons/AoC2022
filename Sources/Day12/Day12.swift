05bda92f5fdad9e6f5882e910ef1d370c2679f54
Gereon Steffens <gereon@steffens.org>
2022-12-12 12:12:56 +0100

    day 12

--- Sources/Day12/Day12.swift
+++ Sources/Day12/Day12.swift
@@ -6,17 +6,70 @@
 
 import AoCTools
 
+private struct HeightMap {
+    let map: [Point: UInt8]
+    let start: Point
+    let destination: Point
+}
+
 final class Day12: AOCDay {
+    private let heightMap: HeightMap
 
     init(rawInput: String? = nil) {
-        // let input = rawInput ?? Self.rawInput
+        let input = rawInput ?? Self.rawInput
+
+        var start = Point.zero
+        var destination = Point.zero
+        var map = [Point: UInt8]()
+        for (y, line) in input.lines.enumerated() {
+            for (x, ch) in line.enumerated() {
+                var ch = ch
+                let point = Point(x, y)
+                if ch == "S" {
+                    start = point
+                    ch = "a"
+                }
+                if ch == "E" {
+                    destination = point
+                    ch = "z"
+                }
+                map[point] = ch.asciiValue!
+            }
+        }
+        self.heightMap = HeightMap(map: map, start: start, destination: destination)
     }
 
     func part1() -> Int {
-        return 0
+        let pathfinder = AStarPathfinder(map: heightMap)
+        let path = pathfinder.shortestPath(from: heightMap.start, to: heightMap.destination)
+        return path.count
     }
 
     func part2() -> Int {
-        return 0
+        let pathfinder = AStarPathfinder(map: heightMap)
+        let starts = heightMap.map.filter { $0.value == 97 }.map { $0.key }
+
+        var shortest = Int.max
+        for start in starts {
+            let path = pathfinder.shortestPath(from: start, to: heightMap.destination)
+            if !path.isEmpty {
+                shortest = min(shortest, path.count)
+            }
+        }
+        return shortest
+    }
+}
+
+extension HeightMap: Pathfinding {
+    func neighbors(for point: Point) -> [Point] {
+        let height = map[point]! + 1
+        return point
+            .neighbors()
+            .filter {
+                if let h = map[$0] {
+                    return h <= height
+                }
+                return false
+            }
     }
 }
--- Tests/Day12Tests.swift
+++ Tests/Day12Tests.swift
@@ -4,13 +4,23 @@ import XCTest
 final class Day12Tests: XCTestCase {
     func testDay12_1() throws {
         let day = Day12(rawInput: """
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
 """)
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 31)
     }
 
     func testDay12_2() throws {
         let day = Day12(rawInput: """
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
 """)
-        XCTAssertEqual(day.part2(), 0)
+        XCTAssertEqual(day.part2(), 29)
     }
 }
