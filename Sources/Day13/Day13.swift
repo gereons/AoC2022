ceb8ba3dc96494db20d112358938a7b244215a62
Gereon Steffens <gereon@steffens.org>
2022-12-14 17:39:28 +0100

    day 13

--- Sources/Day13/Day13.swift
+++ Sources/Day13/Day13.swift
@@ -7,133 +7,99 @@
 import AoCTools
 import Foundation
 
+private enum Data {
+    case int(Int)
+    indirect case list([Data])
+}
+
+extension Data: CustomStringConvertible {
+    var description: String {
+        var descr = ""
+        switch self {
+        case .int(let int):
+            descr.append("\(int),")
+        case .list(let list):
+            descr.append("[")
+            list.forEach {
+                descr.append($0.description)
+            }
+            descr.append("]")
+        }
+        return descr
+    }
+}
+
+// thanks to /u/mayoff on reddit for pointing out how `Decodable` can parse the input
+// https://www.reddit.com/r/adventofcode/comments/zkmyh4/2022_day_13_solutions/j00oyl6/?context=3
+extension Data: Decodable {
+    init(from decoder: Decoder) throws {
+        do {
+            let c = try decoder.singleValueContainer()
+            self = .int(try c.decode(Int.self))
+        } catch {
+            self = .list(try [Data](from: decoder))
+        }
+    }
+}
+
+extension Data: Comparable {
+    static func < (lhs: Data, rhs: Data) -> Bool {
+        switch (lhs, rhs) {
+        case (.int(let i1), .int(let i2)):
+            return i1 < i2
+        case (.list, .int(let i2)):
+            return lhs < .list([.int(i2)])
+        case (.int(let i1), .list):
+            return .list([.int(i1)]) < rhs
+        case (.list(let l1), .list(let l2)):
+            for zipped in zip(l1, l2) {
+                if zipped.0 < zipped.1 { return true }
+                if zipped.0 > zipped.1 { return false }
+            }
+            return l1.count < l2.count
+        }
+    }
+}
+
 final class Day13: AOCDay {
+    private let packets: [Pair<Data, Data>]
+
     init(rawInput: String? = nil) {
         let input = rawInput ?? Self.rawInput
 
-        for str in input.lines where !str.isEmpty {
-            let list = Parser.createList(from: str)
-            print(str, list)
+        var packets = [Pair<Data, Data>]()
+        let decoder = JSONDecoder()
+        for lines in input.lines.split(whereSeparator: \.isEmpty) {
+            let d1 = try! decoder.decode(Data.self, from: lines.first!.data(using: .utf8)!)
+            let d2 = try! decoder.decode(Data.self, from: lines.last!.data(using: .utf8)!)
+            packets.append(Pair(d1, d2))
         }
+
+        self.packets = packets
     }
     
     func part1() -> Int {
-        return 0
+        var count = 0
+        for (index, packet) in packets.enumerated() {
+            let less = packet.first < packet.second
+            if less {
+                count += index + 1
+            }
+        }
+        return count
     }
 
     func part2() -> Int {
-        return 0
+        var allData = packets.flatMap { [$0.first, $0.second] }
+
+        let special1 = Data.list([.list([.int(2)])])
+        let special2 = Data.list([.list([.int(6)])])
+        allData.append(special1)
+        allData.append(special2)
+        allData.sort(by: <)
+
+        let pos1 = allData.firstIndex(of: special1)!
+        let pos2 = allData.firstIndex(of: special2)!
+        return (pos1 + 1) * (pos2 + 1)
     }
 }
-
-extension Day13 {
-    struct List: Equatable {
-        let integers: [Int]
-        let lists: [List]
-
-        init(integers: [Int] = [], lists: [List] = []) {
-            self.integers = integers
-            self.lists = lists
-        }
-    }
-
-    enum TokenType {
-        case obracket
-        case cbracket
-        case integer
-        case comma
-    }
-
-    struct Token {
-        let type: TokenType
-        let value: Int?
-    }
-
-    struct Tokenizer {
-        static func tokenize(packet: String) -> [Token] {
-            var tokens = [Token]()
-            var idx = 0
-            while idx < packet.count {
-                let ch = packet[idx]
-                switch ch {
-                case "[": tokens.append(Token(type: .obracket, value: nil))
-                case "]": tokens.append(Token(type: .cbracket, value: nil))
-                case ",": tokens.append(Token(type: .comma, value: nil))
-                case "0"..."9":
-                    let value = Int(String(ch))!
-                    while
-                    tokens.append(Token(type: .integer, value: int))
-                default:
-                    continue
-                }
-            }
-            return tokens
-        }
-    }
-
-    class Parser {
-        private var tokens: [Token]
-
-        init(tokens: [Token]) {
-            self.tokens = tokens
-        }
-
-        static func createList(from packet: String) -> List {
-            let parser = Parser(tokens: Tokenizer.tokenize(packet: packet))
-            return parser.list()
-        }
-
-        private func list() -> List {
-            var integers = [Int]()
-            var lists = [List]()
-
-            consume(.obracket)
-            if peek(.integer) {
-                integers = ints()
-            }
-            if peek(.obracket) {
-                
-            }
-            consume(.cbracket)
-
-            return List(integers: integers, lists: lists)
-        }
-
-        private func ints() -> [Int] {
-            var value = 0
-            var ints = [Int]()
-
-            while true {
-                while peek(.integer) {
-                    value *= 10
-                    let i = Int(String(consume(.integer).value))!
-                    value += i
-                }
-                ints.append(value)
-                value = 0
-                if peek(.comma) {
-                    consume(.comma)
-                } else {
-                    break
-                }
-            }
-
-            return ints
-        }
-
-        private func peek(_ expected: TokenType) -> Bool {
-            guard !tokens.isEmpty else { return false }
-            return tokens[0].type == expected
-        }
-
-        @discardableResult
-        private func consume(_ expected: TokenType) -> Token {
-            if tokens[0].type == expected {
-                return tokens.remove(at: 0)
-            } else {
-                fatalError("expected \(expected), got \(tokens[0].type)")
-            }
-        }
-    }
-}
-*/
--- Tests/Day13Tests.swift
+++ Tests/Day13Tests.swift
@@ -28,35 +28,13 @@ final class Day13Tests: XCTestCase {
 [1,[2,[3,[4,[5,6,0]]]],8,9]
 """
 
-    func testDay13_parse() throws {
-//        typealias List = Day13.List
-//        typealias Parser = Day13.Parser
-//        XCTAssertEqual(Parser.createList(from: "[1,1,3,1,1]"), List(integers: [1,1,3,1,1]))
-//        XCTAssertEqual(Parser.createList(from: "[1,1,5,1,1]"), List(integers: [1,1,5,1,1]))
-//        XCTAssertEqual(Parser.createList(from: "[[1],[2,3,4]]"), List(lists: [List(integers: [1]), List(integers: [2,3,4])]))
-//        XCTAssertEqual(Parser.createList(from: "[[1],4]"), foo)
-//        XCTAssertEqual(Parser.createList(from: "[9]"), foo)
-//        XCTAssertEqual(Parser.createList(from: "[[8,7,6]]"), foo)
-//        XCTAssertEqual(Parser.createList(from: "[[4,4],4,4]"), foo)
-//        XCTAssertEqual(Parser.createList(from: "[[4,4],4,4,4]"), foo)
-//        XCTAssertEqual(Parser.createList(from: "[7,7,7,7]"), foo)
-//        XCTAssertEqual(Parser.createList(from: "[7,7,7]"), foo)
-//        XCTAssertEqual(Parser.createList(from: "[]"), foo)
-//        XCTAssertEqual(Parser.createList(from: "[3]"), foo)
-//        XCTAssertEqual(Parser.createList(from: "[[[]]]"), foo)
-//        XCTAssertEqual(Parser.createList(from: "[[]]"), foo)
-//        XCTAssertEqual(Parser.createList(from: "[1,[2,[3,[4,[5,6,7]]]],8,9]"), foo)
-//        XCTAssertEqual(Parser.createList(from: "[1,[2,[3,[4,[5,6,0]]]],8,9]"), foo)
-    }
-
     func testDay13_1() throws {
         let day = Day13(rawInput: input)
         XCTAssertEqual(day.part1(), 13)
     }
 
     func testDay13_2() throws {
-        let day = Day13(rawInput: """
-""")
-        XCTAssertEqual(day.part2(), 0)
+        let day = Day13(rawInput: input)
+        XCTAssertEqual(day.part2(), 140)
     }
 }
