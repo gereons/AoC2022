d24fa51cbd8ace826e53c78f891a24cb9960930f
Gereon Steffens <gereon@steffens.org>
2022-12-14 10:37:25 +0100

    day 13 wip

--- Sources/Day13/Day13.swift
+++ Sources/Day13/Day13.swift
@@ -5,11 +5,16 @@
 //
 
 import AoCTools
+import Foundation
 
 final class Day13: AOCDay {
-
     init(rawInput: String? = nil) {
-        // let input = rawInput ?? Self.rawInput
+        let input = rawInput ?? Self.rawInput
+
+        for str in input.lines where !str.isEmpty {
+            let list = Parser.createList(from: str)
+            print(str, list)
+        }
     }
     
     func part1() -> Int {
@@ -20,3 +25,115 @@ final class Day13: AOCDay {
         return 0
     }
 }
+
+extension Day13 {
+    struct List: Equatable {
+        let integers: [Int]
+        let lists: [List]
+
+        init(integers: [Int] = [], lists: [List] = []) {
+            self.integers = integers
+            self.lists = lists
+        }
+    }
+
+    enum TokenType {
+        case obracket
+        case cbracket
+        case integer
+        case comma
+    }
+
+    struct Token {
+        let type: TokenType
+        let value: Int?
+    }
+
+    struct Tokenizer {
+        static func tokenize(packet: String) -> [Token] {
+            var tokens = [Token]()
+            var idx = 0
+            while idx < packet.count {
+                let ch = packet[idx]
+                switch ch {
+                case "[": tokens.append(Token(type: .obracket, value: nil))
+                case "]": tokens.append(Token(type: .cbracket, value: nil))
+                case ",": tokens.append(Token(type: .comma, value: nil))
+                case "0"..."9":
+                    let value = Int(String(ch))!
+                    while
+                    tokens.append(Token(type: .integer, value: int))
+                default:
+                    continue
+                }
+            }
+            return tokens
+        }
+    }
+
+    class Parser {
+        private var tokens: [Token]
+
+        init(tokens: [Token]) {
+            self.tokens = tokens
+        }
+
+        static func createList(from packet: String) -> List {
+            let parser = Parser(tokens: Tokenizer.tokenize(packet: packet))
+            return parser.list()
+        }
+
+        private func list() -> List {
+            var integers = [Int]()
+            var lists = [List]()
+
+            consume(.obracket)
+            if peek(.integer) {
+                integers = ints()
+            }
+            if peek(.obracket) {
+                
+            }
+            consume(.cbracket)
+
+            return List(integers: integers, lists: lists)
+        }
+
+        private func ints() -> [Int] {
+            var value = 0
+            var ints = [Int]()
+
+            while true {
+                while peek(.integer) {
+                    value *= 10
+                    let i = Int(String(consume(.integer).value))!
+                    value += i
+                }
+                ints.append(value)
+                value = 0
+                if peek(.comma) {
+                    consume(.comma)
+                } else {
+                    break
+                }
+            }
+
+            return ints
+        }
+
+        private func peek(_ expected: TokenType) -> Bool {
+            guard !tokens.isEmpty else { return false }
+            return tokens[0].type == expected
+        }
+
+        @discardableResult
+        private func consume(_ expected: TokenType) -> Token {
+            if tokens[0].type == expected {
+                return tokens.remove(at: 0)
+            } else {
+                fatalError("expected \(expected), got \(tokens[0].type)")
+            }
+        }
+    }
+}
+*/
--- Tests/Day13Tests.swift
+++ Tests/Day13Tests.swift
@@ -2,10 +2,56 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day13Tests: XCTestCase {
+    let input = """
+[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+"""
+
+    func testDay13_parse() throws {
+//        typealias List = Day13.List
+//        typealias Parser = Day13.Parser
+//        XCTAssertEqual(Parser.createList(from: "[1,1,3,1,1]"), List(integers: [1,1,3,1,1]))
+//        XCTAssertEqual(Parser.createList(from: "[1,1,5,1,1]"), List(integers: [1,1,5,1,1]))
+//        XCTAssertEqual(Parser.createList(from: "[[1],[2,3,4]]"), List(lists: [List(integers: [1]), List(integers: [2,3,4])]))
+//        XCTAssertEqual(Parser.createList(from: "[[1],4]"), foo)
+//        XCTAssertEqual(Parser.createList(from: "[9]"), foo)
+//        XCTAssertEqual(Parser.createList(from: "[[8,7,6]]"), foo)
+//        XCTAssertEqual(Parser.createList(from: "[[4,4],4,4]"), foo)
+//        XCTAssertEqual(Parser.createList(from: "[[4,4],4,4,4]"), foo)
+//        XCTAssertEqual(Parser.createList(from: "[7,7,7,7]"), foo)
+//        XCTAssertEqual(Parser.createList(from: "[7,7,7]"), foo)
+//        XCTAssertEqual(Parser.createList(from: "[]"), foo)
+//        XCTAssertEqual(Parser.createList(from: "[3]"), foo)
+//        XCTAssertEqual(Parser.createList(from: "[[[]]]"), foo)
+//        XCTAssertEqual(Parser.createList(from: "[[]]"), foo)
+//        XCTAssertEqual(Parser.createList(from: "[1,[2,[3,[4,[5,6,7]]]],8,9]"), foo)
+//        XCTAssertEqual(Parser.createList(from: "[1,[2,[3,[4,[5,6,0]]]],8,9]"), foo)
+    }
+
     func testDay13_1() throws {
-        let day = Day13(rawInput: """
-""")
-        XCTAssertEqual(day.part1(), 0)
+        let day = Day13(rawInput: input)
+        XCTAssertEqual(day.part1(), 13)
     }
 
     func testDay13_2() throws {
