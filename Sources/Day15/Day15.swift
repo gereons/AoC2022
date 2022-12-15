ee4bdd394f444941c001ae2fd1da4f22d76a4c14
Gereon Steffens <gereon@steffens.org>
2022-12-15 12:25:17 +0100

    day 15 wip

--- Sources/Day15/Day15.swift
+++ Sources/Day15/Day15.swift
@@ -5,18 +5,192 @@
 //
 
 import AoCTools
+import RegexBuilder
+
+private struct Sensor {
+    let position: Point
+    let beacon: Point
+    let range: Int
+
+    init(_ str: String) {
+        // Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+        let number = Capture {
+            Optionally("-")
+            ZeroOrMore(.digit)
+        } transform: { Int($0)! }
+
+        let regex = Regex {
+            "Sensor at x="
+            number
+            ", y="
+            number
+            ": closest beacon is at x="
+            number
+            ", y="
+            number
+        }
+
+        let matches = try! regex.wholeMatch(in: str)!.output
+        position = Point(matches.1, matches.2)
+        beacon = Point(matches.3, matches.4)
+        range = position.distance(to: beacon)
+    }
+}
+
+private enum Tile: Character, Drawable {
+    case sensor = "S"
+    case beacon = "B"
+    case empty = "#"
+}
 
 final class Day15: AOCDay {
+    private let sensors: [Sensor]
+    private let row: Int
+    private let size: Int
 
-    init(rawInput: String? = nil) {
-        // let input = rawInput ?? Self.rawInput
+    convenience init(rawInput: String? = nil) {
+        self.init(rawInput: rawInput, row: 2000000, size: 4000000)
+    }
+
+    init(rawInput: String? = nil, row: Int, size: Int) {
+        let input = rawInput ?? Self.rawInput
+
+        self.row = row
+        self.size = size
+        self.sensors = input.lines.map { Sensor($0) }
     }
 
     func part1() -> Int {
-        return 0
+        var grid = [Point: Tile]()
+        var maxX = Int.min
+        var minX = Int.max
+        for sensor in sensors {
+            grid[sensor.position] = .sensor
+            grid[sensor.beacon] = .beacon
+            maxX = max(maxX, sensor.position.x)
+            minX = min(minX, sensor.position.x)
+        }
+        let leftRange = sensors.filter { $0.position.x == minX }.max(of: \.range)!
+        let rightRange = sensors.filter { $0.position.x == maxX }.max(of: \.range)!
+
+        var count = 0
+        for x in minX - leftRange ... maxX + rightRange {
+            let point = Point(x, row)
+            if isInRange(point, sensors) {
+                count += 1
+            }
+        }
+
+        return count
     }
 
+    private func isInRange(_ point: Point, _ sensors: [Sensor]) -> Bool {
+        for sensor in sensors {
+            if point == sensor.position { return false }
+            if point == sensor.beacon { return false }
+            if point.distance(to: sensor.position) <= sensor.range { return true }
+        }
+        return false
+    }
+
+
     func part2() -> Int {
+        var grid = [Point: Tile]()
+        var maxX = Int.min
+        var minX = Int.max
+        for sensor in sensors {
+            grid[sensor.position] = .sensor
+            grid[sensor.beacon] = .beacon
+            maxX = max(maxX, sensor.position.x)
+            minX = min(minX, sensor.position.x)
+        }
+        let leftRange = sensors.filter { $0.position.x == minX }.max(of: \.range)!
+        let rightRange = sensors.filter { $0.position.x == maxX }.max(of: \.range)!
+
+        var missing = Point.zero
+        for y in 0 ... size {
+            print(y)
+            for x in max(0, minX - leftRange) ... min(size, maxX + rightRange) {
+                let point = Point(x, y)
+                if isDistress(point, sensors) {
+                    missing = point
+                    break
+                }
+            }
+        }
+
+        return missing.x * 4000000 + missing.y
+    }
+
+    private func isDistress(_ point: Point, _ sensors: [Sensor]) -> Bool {
+        for sensor in sensors {
+            if point == sensor.position { return false }
+            if point == sensor.beacon { return false }
+            if point.distance(to: sensor.position) <= sensor.range { return false }
+        }
+        return true
+    }
+}
+
+extension Day15 {
+    func part1_naive() -> Int {
+        var grid = [Point: Tile]()
+        for sensor in sensors {
+            grid[sensor.position] = .sensor
+            grid[sensor.beacon] = .beacon
+        }
+
+        for sensor in sensors {
+            let range = sensor.range + 1
+            for x in sensor.position.x - range ... sensor.position.x + range {
+                for y in sensor.position.y - range ... sensor.position.y + range {
+                    let p = Point(x, y)
+                    if p.distance(to: sensor.position) < range && grid[p] == nil {
+                        grid[p] = .empty
+                    }
+                }
+            }
+        }
+
+        // Grid(points: grid).draw()
+        let minX = grid.keys.min(of: \.x)!
+        let maxX = grid.keys.max(of: \.x)!
+
+        var count = 0
+        for x in minX ... maxX {
+            if grid[Point(x, row)] == .empty {
+                count += 1
+            }
+        }
+        return count
+    }
+
+    func part2_naive() -> Int {
+        var grid = [Point: Tile]()
+
+        for sensor in sensors {
+            grid[sensor.position] = .sensor
+            grid[sensor.beacon] = .beacon
+        }
+
+        for sensor in sensors {
+            let range = sensor.range + 1
+            for x in sensor.position.x - range ... sensor.position.x + range {
+                for y in sensor.position.y - range ... sensor.position.y + range {
+                    let p = Point(x, y)
+                    if p.distance(to: sensor.position) < range && grid[p] == nil {
+                        grid[p] = .empty
+                    }
+                }
+            }
+        }
+
+        grid = grid
+            .filter {
+                $0.key.x >= 0 && $0.key.x <= size &&
+                $0.key.y >= 0 && $0.key.y <= size
+            }
+
         return 0
     }
 }
--- Tests/Day15Tests.swift
+++ Tests/Day15Tests.swift
@@ -2,15 +2,30 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day15Tests: XCTestCase {
+    let input = """
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
+"""
+
     func testDay15_1() throws {
-        let day = Day15(rawInput: """
-""")
-        XCTAssertEqual(day.part1(), 0)
+        let day = Day15(rawInput: input, row: 10, size: 20)
+        XCTAssertEqual(day.part1(), 26)
     }
 
     func testDay15_2() throws {
-        let day = Day15(rawInput: """
-""")
-        XCTAssertEqual(day.part2(), 0)
+        let day = Day15(rawInput: input, row: 10, size: 20)
+        XCTAssertEqual(day.part2(), 56000011)
     }
 }
