ba9a03b3c9c6ef38fe8cbd6b4fd66ea73d350266
Gereon Steffens <gereon@steffens.org>
2022-12-17 11:03:00 +0100

    day 16 wip

--- Sources/Day16/Day16.swift
+++ Sources/Day16/Day16.swift
@@ -5,18 +5,115 @@
 //
 
 import AoCTools
+import RegexBuilder
+
+private struct Valve: Equatable {
+    let id: String
+    let flowRate: Int
+    let leadsTo: [String]
+
+    init(_ str: String) {
+        // Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+        // Valve AA has flow rate=0; tunnel leads to valve DD
+        let regex = Regex {
+            "Valve "
+            Capture(ZeroOrMore(.word)) { String($0) }
+            " has flow rate="
+            Capture(ZeroOrMore(.digit)) { Int($0)! }
+            ChoiceOf {
+                "; tunnels lead to valves "
+                "; tunnel leads to valve "
+            }
+            Capture(ZeroOrMore(.any)) { String($0) }
+        }
+
+        let matches = try! regex.wholeMatch(in: str)!.output
+        id = matches.1
+        flowRate = matches.2
+        leadsTo = matches.3.components(separatedBy: ", ")
+    }
+}
 
 final class Day16: AOCDay {
+    private let valves: [String: Valve]
+    private let maxOpen: Int
 
     init(rawInput: String? = nil) {
-        // let input = rawInput ?? Self.rawInput
+        let input = rawInput ?? Self.rawInput
+
+        valves = input.lines.map { Valve($0) }.mapped(by: \.id)
+        maxOpen = valves.values.count { $0.flowRate > 0 }
     }
 
     func part1() -> Int {
-        return 0
+        let aa = valves["AA"]!
+
+        return openValves(startAt: aa)
     }
 
     func part2() -> Int {
         return 0
+    }
+
+    private func openValves(startAt: Valve) -> Int {
+        var results = [[Open]]()
+
+        openValves(startAt: startAt, minute: 1, open: [], results: &results)
+
+        var maxPressure = Int.min
+        var maxResult = [Open]()
+        for result in results {
+            var pressure = 0
+            for op in result {
+                pressure += op.flowRate * (30 - op.minute)
+            }
+            // maxPressure = max(maxPressure, pressure)
+            if pressure > maxPressure {
+                maxPressure = pressure
+                maxResult = result
+            }
+        }
+
+        return maxPressure
+    }
+
+    private struct Open: Hashable {
+        let valve: String
+        let flowRate: Int
+        let minute: Int
+    }
+
+    private struct State: Hashable {
+        let openValves: [Open]
+        let current: String
+        let next: String
+    }
+
+    private var seen = Set<State>()
+
+    private func openValves(startAt valve: Valve, minute: Int, open: [Open], results: inout [[Open]]) {
+        if minute > 30 || open.count == maxOpen {
+            results.append(open)
+            return
+        }
+
+        var open = open
+        var minute = minute
+        if valve.flowRate > 0 {
+            let isOpen = open.contains { $0.valve == valve.id }
+            if !isOpen {
+                minute += 1
+                open.append(Open(valve: valve.id, flowRate: valve.flowRate, minute: minute))
+            }
+        }
+
+        for goto in valve.leadsTo {
+            let target = valves[goto]!
+            let state = State(openValves: open, current: valve.id, next: target.id)
+            if !seen.contains(state) {
+                seen.insert(state)
+                openValves(startAt: target, minute: minute + 1, open: open, results: &results)
+            }
+        }
     }
 }
--- Tests/Day16Tests.swift
+++ Tests/Day16Tests.swift
@@ -4,13 +4,23 @@ import XCTest
 final class Day16Tests: XCTestCase {
     func testDay16_1() throws {
         let day = Day16(rawInput: """
+Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+Valve BB has flow rate=13; tunnels lead to valves CC, AA
+Valve CC has flow rate=2; tunnels lead to valves DD, BB
+Valve DD has flow rate=20; tunnels lead to valves CC, AA, EE
+Valve EE has flow rate=3; tunnels lead to valves FF, DD
+Valve FF has flow rate=0; tunnels lead to valves EE, GG
+Valve GG has flow rate=0; tunnels lead to valves FF, HH
+Valve HH has flow rate=22; tunnel leads to valve GG
+Valve II has flow rate=0; tunnels lead to valves AA, JJ
+Valve JJ has flow rate=21; tunnel leads to valve II
 """)
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 1651)
     }
 
-    func testDay16_2() throws {
-        let day = Day16(rawInput: """
-""")
-        XCTAssertEqual(day.part2(), 0)
-    }
+//    func testDay16_2() throws {
+//        let day = Day16(rawInput: """
+//""")
+//        XCTAssertEqual(day.part2(), 0)
+//    }
 }
