a8968f0056d2f6a51f4e331ac98d598964ecf814
Gereon Steffens <gereon@steffens.org>
2022-12-17 16:17:03 +0100

    day 17 part 2

--- Sources/Day17/Day17.swift
+++ Sources/Day17/Day17.swift
@@ -6,12 +6,6 @@
 
 import AoCTools
 
-private enum Unit: Character, Drawable {
-    case rock = "#"
-    case wall = "|"
-    case air = "."
-}
-
 private struct Shape {
     // top left is at (0, 0)
     let points: [Point]
@@ -34,34 +28,28 @@ private struct Shape {
     static let ell = Shape(points: [Point(2,0), Point(2,1), Point(2,2), Point(1,2), Point(0,2)], height: 3)
     static let beam = Shape(points: [Point(0,0), Point(0,1), Point(0,2), Point(0,3)], height: 4)
     static let block = Shape(points: [Point(0,0), Point(0,1), Point(1,0), Point(1,1)], height: 2)
-
-    static let shapes = [ dash, plus, ell, beam, block ]
-
-    static func get(_ block: Int) -> Shape {
-        shapes[block % shapes.count]
-    }
 }
 
-private struct Tunnel {
-    private(set) var grid = [Point: Unit]()
+private struct Playfield {
+    private(set) var grid = [Point: Bool]()
     private(set) var minY: Int
 
     var accumulatedHeight: Int { -minY + 1 }
 
     init() {
         minY = 1
-        add(Point(-1, 1).line(to: Point(7,1)), .rock)
+        add(Point(-1, 1).line(to: Point(7,1)))
     }
 
     mutating func add(_ shape: Shape) {
-        add(shape.points, .rock)
+        add(shape.points)
         self.minY = min(self.minY, shape.points.min(of: \.y)!)
     }
 
-    mutating func add(_ points: [Point], _ unit: Unit) {
+    mutating func add(_ points: [Point]) {
         points.forEach {
             assert(grid[$0] == nil)
-            grid[$0] = unit
+            grid[$0] = true
         }
     }
 
@@ -71,14 +59,35 @@ private struct Tunnel {
         }
     }
 
+    // highest point in every column, normalized so that the lowest point is at y==0
+    func skyline() -> [Point] {
+        var maxY = Int.min
+        let points = (0...6).map { x in
+            let y = grid.keys.filter { $0.x == x }.min(of: \.y) ?? 0
+            maxY = max(maxY, y)
+            return Point(x, y)
+        }
+
+        return points.map { $0.moved(to: .s, steps: -maxY)}
+    }
+
     func draw() {
         Grid(points: grid).draw()
     }
 }
 
+private struct Key: Hashable {
+    let skyline: [Point]
+    let shapeIndex: Int
+    let jetIndex: Int
+}
+
 final class Day17: AOCDay {
     let jets: [Character]
     private var jetIndex = 0
+
+    private let shapes: [Shape] = [ .dash, .plus, .ell, .beam, .block ]
+    private var shapeIndex = 0
 
     init(rawInput: String? = nil) {
         let input = rawInput ?? Self.rawInput
@@ -86,45 +95,79 @@ final class Day17: AOCDay {
     }
 
     func part1() -> Int {
-        var tunnel = Tunnel()
+        jetIndex = 0
+        shapeIndex = 0
 
-        for block in 0..<2022 {
-            var shape = Shape.get(block)
-            // set start position
-            shape = shape
-                .moved(to: .n, tunnel.accumulatedHeight + 2 + shape.height)
-                .moved(to: .e, 2)
-
-            var atRest = false
-            while !atRest {
-                // jet move
-                let movedByJet = shape.moved(to: jet())
-                if tunnel.possiblePlacement(movedByJet) {
-                    shape = movedByJet
-                }
-
-                // drop down
-                let down = shape.moved(to: .s)
-                if tunnel.possiblePlacement(down) {
-                    shape = down
-                } else {
-                    atRest = true
-                }
-            }
-            tunnel.add(shape)
+        var playfield = Playfield()
+        for _ in 0..<2022 {
+            playTetris(in: &playfield)
         }
-
-        return tunnel.accumulatedHeight
+        return playfield.accumulatedHeight
     }
 
     func part2() -> Int {
+        jetIndex = 0
+        shapeIndex = 0
+        let rocks = 1_000_000_000_000 - 1
+        var seen = [Key: (Int, Int)]()
+
+        var playfield = Playfield()
+        for block in 0..<Int.max {
+            playTetris(in: &playfield)
+            let key = Key(skyline: playfield.skyline(), shapeIndex: shapeIndex, jetIndex: jetIndex)
+            if let (start, height) = seen[key] {
+                let heightGain = playfield.accumulatedHeight - height
+                let loopLen = block - start
+                let loops = (rocks - start) / loopLen
+                let remainder = (rocks - start) - (loops * loopLen)
+                for _ in 0..<remainder {
+                    playTetris(in: &playfield)
+                }
+
+                // -1 loops because we've stopped at the end of the first
+                return playfield.accumulatedHeight + ((loops - 1) * heightGain)
+            }
+            seen[key] = (block, playfield.accumulatedHeight)
+        }
+
         return 0
     }
 
-    func jet() -> Point.Direction {
-        let ch = jets[jetIndex % jets.count]
-        jetIndex += 1
+    private func playTetris(in playfield: inout Playfield) {
+        var shape = nextShape()
+        // set start position
+        shape = shape
+            .moved(to: .n, playfield.accumulatedHeight + 2 + shape.height)
+            .moved(to: .e, 2)
+
+        var atRest = false
+        while !atRest {
+            // jet move
+            let movedByJet = shape.moved(to: nextJet())
+            if playfield.possiblePlacement(movedByJet) {
+                shape = movedByJet
+            }
+
+            // drop down
+            let down = shape.moved(to: .s)
+            if playfield.possiblePlacement(down) {
+                shape = down
+            } else {
+                atRest = true
+            }
+        }
+        playfield.add(shape)
+    }
+
+    private func nextJet() -> Point.Direction {
+        defer { jetIndex = (jetIndex + 1) % jets.count }
+        let ch = jets[jetIndex]
         return ch == "<" ? .w : .e
+    }
+
+    private func nextShape() -> Shape {
+        defer { shapeIndex = (shapeIndex + 1) % shapes.count }
+        return shapes[shapeIndex]
     }
 }
 
--- Tests/Day17Tests.swift
+++ Tests/Day17Tests.swift
@@ -2,14 +2,15 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day17Tests: XCTestCase {
+    let input = ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>"
+
     func testDay17_1() throws {
-        let day = Day17(rawInput: ">>><<><>><<<>><>>><<<>>><<<><<<>><>><<>>")
+        let day = Day17(rawInput: input)
         XCTAssertEqual(day.part1(), 3068)
     }
 
-//    func testDay17_2() throws {
-//        let day = Day17(rawInput: """
-//""")
-//        XCTAssertEqual(day.part2(), 0)
-//    }
+    func testDay17_2() throws {
+        let day = Day17(rawInput: input)
+        XCTAssertEqual(day.part2(), 1514285714288)
+    }
 }
