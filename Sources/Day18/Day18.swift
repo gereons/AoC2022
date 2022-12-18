0e25baa8dc5866078e39ab3086eff7a0ea83d831
Gereon Steffens <gereon@steffens.org>
2022-12-18 13:16:15 +0100

    day 18

--- Sources/Day18/Day18.swift
+++ Sources/Day18/Day18.swift
@@ -6,17 +6,82 @@
 
 import AoCTools
 
+private class Cube {
+    enum Contents {
+        case water, lava
+    }
+    let minX, maxX, minY, maxY, minZ, maxZ: Int
+
+    var points: [Point3: Contents]
+
+    init(points: [Point3]) {
+        minX = points.min(of: \.x)! - 1
+        maxX = points.max(of: \.x)! + 1
+        minY = points.min(of: \.y)! - 1
+        maxY = points.max(of: \.y)! + 1
+        minZ = points.min(of: \.z)! - 1
+        maxZ = points.max(of: \.z)! + 1
+
+        let lava = [Contents](repeating: .lava, count: points.count)
+        self.points = Dictionary(uniqueKeysWithValues: zip(points, lava))
+    }
+
+    var start: Point3 { Point3(minX, minY, minZ) }
+
+    func contains(_ point: Point3) -> Bool {
+        point.x >= minX && point.x <= maxX &&
+        point.y >= minY && point.y <= maxY &&
+        point.z >= minZ && point.z <= maxZ
+    }
+}
+
 final class Day18: AOCDay {
+    let droplet: [Point3]
 
     init(rawInput: String? = nil) {
-        // let input = rawInput ?? Self.rawInput
+        let input = rawInput ?? Self.rawInput
+        droplet = input.lines.map { line in
+            let parts = line.components(separatedBy: ",")
+            return Point3(Int(parts[0])!, Int(parts[1])!, Int(parts[2])!)
+        }
     }
 
     func part1() -> Int {
-        return 0
+        let sixes = [Int](repeating: 6, count: droplet.count)
+        var sides = Dictionary(uniqueKeysWithValues: zip(droplet, sixes))
+
+        for cube in droplet {
+            let neighbors = cube.neighbors().filter { sides[$0] != nil }
+            sides[cube]! -= neighbors.count
+        }
+
+        return sides.values.reduce(0, +)
     }
 
     func part2() -> Int {
-        return 0
+        let cube = Cube(points: droplet)
+
+        floodFill(cube, from: cube.start)
+
+        let water = cube.points.filter { $0.value == .water }.keys
+
+        var sides = 0
+        for p in water {
+            let n = p.neighbors().filter { cube.points[$0] == .lava }
+            sides += n.count
+        }
+
+        return sides
+    }
+
+    private func floodFill(_ cube: Cube, from start: Point3) {
+        cube.points[start] = .water
+
+        let neighbors = start.neighbors()
+            .filter { cube.contains($0) && cube.points[$0] == nil }
+        
+        for n in neighbors {
+            floodFill(cube, from: n)
+        }
     }
 }
--- Tests/Day18Tests.swift
+++ Tests/Day18Tests.swift
@@ -2,15 +2,29 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day18Tests: XCTestCase {
+    let input = """
+2,2,2
+1,2,2
+3,2,2
+2,1,2
+2,3,2
+2,2,1
+2,2,3
+2,2,4
+2,2,6
+1,2,5
+3,2,5
+2,1,5
+2,3,5
+"""
+
     func testDay18_1() throws {
-        let day = Day18(rawInput: """
-""")
-        XCTAssertEqual(day.part1(), 0)
+        let day = Day18(rawInput: input)
+        XCTAssertEqual(day.part1(), 64)
     }
 
     func testDay18_2() throws {
-        let day = Day18(rawInput: """
-""")
-        XCTAssertEqual(day.part2(), 0)
+        let day = Day18(rawInput: input)
+        XCTAssertEqual(day.part2(), 58)
     }
 }
