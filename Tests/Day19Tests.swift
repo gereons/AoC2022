4d2c328aef2bb670ed98948935d118bcc86f278d
Gereon Steffens <gereon@steffens.org>
2022-12-20 08:16:42 +0100

    day 19

--- Tests/Day19Tests.swift
+++ Tests/Day19Tests.swift
@@ -2,15 +2,18 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day19Tests: XCTestCase {
+    let input = """
+Blueprint 1: Each ore robot costs 4 ore. Each clay robot costs 2 ore. Each obsidian robot costs 3 ore and 14 clay. Each geode robot costs 2 ore and 7 obsidian.
+Blueprint 2: Each ore robot costs 2 ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian.
+"""
+
     func testDay19_1() throws {
-        let day = Day19(rawInput: """
-""")
-        XCTAssertEqual(day.part1(), 0)
+        let day = Day19(rawInput: input)
+        XCTAssertEqual(day.part1(), 33)
     }
 
     func testDay19_2() throws {
-        let day = Day19(rawInput: """
-""")
-        XCTAssertEqual(day.part2(), 0)
+        let day = Day19(rawInput: input)
+        XCTAssertEqual(day.part2(), 3472)
     }
 }
