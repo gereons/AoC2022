9e7a4b08a4d50e3e21b4994aab7f905e8264769f
Gereon Steffens <gereon@steffens.org>
2022-12-21 11:45:14 +0100

    day 21 part 1

--- Sources/Day21/Day21.swift
+++ Sources/Day21/Day21.swift
@@ -6,17 +6,60 @@
 
 import AoCTools
 
+private enum Yell {
+    case int(Int)
+    case op(String, String, String)
+}
+
+private struct Monkey {
+    let variable: String
+    let value: Yell
+
+    // root: pppw + sjmn
+    // dbpl: 5
+    init(_ str: String) {
+        let parts = str.components(separatedBy: " ")
+        variable = String(parts[0].dropLast())
+        if let int = Int(parts[1]) {
+            value = .int(int)
+        } else {
+            value = .op(parts[1], parts[2], parts[3])
+        }
+    }
+}
+
 final class Day21: AOCDay {
+    private let monkeys: [String: Monkey]
 
     init(rawInput: String? = nil) {
-        // let input = rawInput ?? Self.rawInput
+        let input = rawInput ?? Self.rawInput
+        monkeys = input.lines.map { Monkey($0) }.mapped(by: \.variable)
     }
 
     func part1() -> Int {
-        return 0
+        let root = monkeys["root"]!
+
+        return evaluate(root)
     }
 
     func part2() -> Int {
         return 0
+    }
+
+    private func evaluate(_ monkey: Monkey) -> Int {
+        switch monkey.value {
+        case .int(let int):
+            return int
+        case .op(let var1, let op, let var2):
+            let v1 = evaluate(monkeys[var1]!)
+            let v2 = evaluate(monkeys[var2]!)
+            switch op {
+            case "+": return v1 + v2
+            case "-": return v1 - v2
+            case "*": return v1 * v2
+            case "/": return v1 / v2
+            default: fatalError()
+            }
+        }
     }
 }
--- Tests/Day21Tests.swift
+++ Tests/Day21Tests.swift
@@ -4,13 +4,28 @@ import XCTest
 final class Day21Tests: XCTestCase {
     func testDay21_1() throws {
         let day = Day21(rawInput: """
+root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32
 """)
-        XCTAssertEqual(day.part1(), 0)
+        XCTAssertEqual(day.part1(), 152)
     }
 
-    func testDay21_2() throws {
-        let day = Day21(rawInput: """
-""")
-        XCTAssertEqual(day.part2(), 0)
-    }
+//    func testDay21_2() throws {
+//        let day = Day21(rawInput: """
+//""")
+//        XCTAssertEqual(day.part2(), 0)
+//    }
 }
