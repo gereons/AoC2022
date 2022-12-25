bc8559361927d902e1543fd356b8b83296c091f0
Gereon Steffens <gereon@steffens.org>
2022-12-25 12:31:37 +0100

    day 25

--- Sources/Day25/Day25.swift
+++ Sources/Day25/Day25.swift
@@ -5,15 +5,40 @@
 //
 
 import AoCTools
+import Foundation
 
 final class Day25: AOCDay {
+    let snafus: [String]
+
+    let toDigit: [Character: Int] = [ "2": 2, "1": 1, "0": 0, "-": -1, "=": -2 ]
+    let fromDigit: [Int: String] =  [ 2: "2", 1: "1", 0: "0", -1: "-", -2: "=" ]
 
     init(rawInput: String? = nil) {
-        // let input = rawInput ?? Self.rawInput
+        let input = rawInput ?? Self.rawInput
+        snafus = input.lines
     }
 
-    func part1() -> Int {
-        return 0
+    func part1() -> String {
+        let sum = snafus.reduce(0) { $0 + toInt($1) }
+        return toSnafu(sum)
+    }
+
+    private func toInt(_ s: String) -> Int {
+        s.reduce(0) {
+            $0 * 5 + toDigit[$1]!
+        }
+    }
+
+    private func toSnafu(_ i: Int) -> String {
+        if i == 0 {
+            return ""
+        }
+        let mod5 = i % 5
+        switch mod5 {
+        case 0, 1, 2: return toSnafu(i / 5) + fromDigit[mod5]!
+        case 3, 4: return toSnafu(i / 5 + 1) + fromDigit[mod5 - 5]!
+        default: fatalError()
+        }
     }
 
     func part2() {}
--- Tests/Day25Tests.swift
+++ Tests/Day25Tests.swift
@@ -2,9 +2,24 @@ import XCTest
 @testable import AdventOfCode
 
 final class Day25Tests: XCTestCase {
+    let input = """
+        1=-0-2
+        12111
+        2=0=
+        21
+        2=01
+        111
+        20012
+        112
+        1=-1=
+        1-12
+        12
+        1=
+        122
+        """
+
     func testDay25_1() throws {
-        let day = Day25(rawInput: """
-""")
-        XCTAssertEqual(day.part1(), 0)
+        let day = Day25(rawInput: input)
+        XCTAssertEqual(day.part1(), "2=-1=0")
     }
 }
