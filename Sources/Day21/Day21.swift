95cf62997ba698f7e17212010ce09e23ee5506c5
Gereon Steffens <gereon@steffens.org>
2023-09-27 12:40:04 +0200

    fix failing test

--- Sources/Day21/Day21.swift
+++ Sources/Day21/Day21.swift
@@ -4,108 +4,166 @@
 // https://adventofcode.com/2022/day/21
 //
 
-import Foundation
 import AoCTools
 
-private enum Yell {
-    case constant(Decimal)
-    case op(String, String, String)
+private class Monkey {
+    let name: String
+
+    init(name: String) {
+        self.name = name
+    }
+
+    var yell: Int {
+        fatalError("must be overridden")
+    }
+
+    func findHumanPath() -> Set<Monkey> {
+        fatalError("must be overridden")
+    }
+
+    func calculateHumanValue(path: Set<Monkey>, incoming: Int) -> Int {
+        fatalError("must be overridden")
+    }
+
+    static func make(_ str: String) -> Monkey {
+        let parts = str.components(separatedBy: " ")
+        let name = String(parts[0].dropLast())
+        if let constant = Int(parts[1]) {
+            return ConstantMonkey(name: name, constant: constant)
+        } else {
+            return CalcMonkey(name: name, leftName: parts[1], op: parts[2], rightName: parts[3])
+        }
+    }
 }
 
-private struct Monkey {
-    let name: String
-    let yell: Yell
+extension Monkey: Hashable {
+    static func == (lhs: Monkey, rhs: Monkey) -> Bool {
+        lhs.name == rhs.name
+    }
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+}
 
-    // root: pppw + sjmn
-    // dbpl: 5
-    init(_ str: String) {
-        let parts = str.components(separatedBy: " ")
-        name = String(parts[0].dropLast())
-        if let int = Int(parts[1]) {
-            yell = .constant(Decimal(int))
-        } else {
-            yell = .op(parts[1], parts[2], parts[3])
+final private class FailMonkey: Monkey { }
+
+final private class ConstantMonkey: Monkey {
+    let constant: Int
+
+    init(name: String, constant: Int) {
+        self.constant = constant
+        super.init(name: name)
+    }
+
+    override var yell: Int {
+        constant
+    }
+
+    override func findHumanPath() -> Set<Monkey> {
+        name == "humn" ? [self] : []
+    }
+
+    override func calculateHumanValue(path: Set<Monkey>, incoming: Int) -> Int {
+        name == "humn" ? incoming : constant
+    }
+}
+
+final private class CalcMonkey: Monkey {
+    let leftName: String
+    let rightName: String
+    let op: String
+    var left: Monkey = FailMonkey(name: "FAIL")
+    var right: Monkey = FailMonkey(name: "FAIL")
+
+    init(name: String, leftName: String, op: String, rightName: String) {
+        self.leftName = leftName
+        self.op = op
+        self.rightName = rightName
+
+        super.init(name: name)
+    }
+
+    override var yell: Int {
+        switch op {
+        case "+": left.yell + right.yell
+        case "-": left.yell - right.yell
+        case "*": left.yell * right.yell
+        case "/": left.yell / right.yell
+        default: fatalError()
         }
     }
 
-    func uses(_ variable: String) -> String? {
-        switch yell {
-        case .op(let v1, _, let v2):
-            if v1 == variable || v2 == variable { return self.name }
-            return nil
-        case .constant: return nil
+    override func findHumanPath() -> Set<Monkey> {
+        let leftPath = left.findHumanPath()
+        let rightPath = right.findHumanPath()
+        if !leftPath.isEmpty {
+            return leftPath + [self]
+        } else if !rightPath.isEmpty {
+            return rightPath + [self]
+        } else {
+            return []
+        }
+    }
+
+    override func calculateHumanValue(path: Set<Monkey>, incoming: Int) -> Int {
+        if name == "root" {
+            if path.contains(left) {
+                return left.calculateHumanValue(path: path, incoming: right.yell)
+            } else {
+                return right.calculateHumanValue(path: path, incoming: left.yell)
+            }
+        } else if path.contains(left) {
+            return left.calculateHumanValue(path: path, incoming: leftReverseOp(incoming, right.yell))
+        } else {
+            return right.calculateHumanValue(path: path, incoming: rightReverseOp(incoming, left.yell))
+        }
+    }
+
+    func leftReverseOp(_ lhs: Int, _ rhs: Int) -> Int {
+        switch self.op {
+        case "+": lhs - rhs
+        case "-": lhs + rhs
+        case "*": lhs / rhs
+        case "/": lhs * rhs
+        default: fatalError()
+        }
+    }
+
+    func rightReverseOp(_ lhs: Int, _ rhs: Int) -> Int {
+        switch self.op {
+        case "+": lhs - rhs
+        case "-": rhs - lhs
+        case "*": lhs / rhs
+        case "/": rhs / lhs
+        default: fatalError()
         }
     }
 }
 
 final class Day21: AOCDay {
-    private let monkeys: [String: Monkey]
+    private let monkeys: Set<Monkey>
+    private let root: Monkey
 
-    init(rawInput: String? = nil) {
-        let input = rawInput ?? Self.rawInput
-        monkeys = input.lines.map { Monkey($0) }.mapped(by: \.name)
+    init(input: String? = nil) {
+        let input = input ?? Self.input
+
+        let allMonkeys = input.lines.map { Monkey.make($0) }.mapped(by: \.name)
+        for monkey in allMonkeys.values.compactMap({ $0 as? CalcMonkey }) {
+            monkey.left = allMonkeys[monkey.leftName]!
+            monkey.right = allMonkeys[monkey.rightName]!
+        }
+
+        monkeys = Set(allMonkeys.values)
+        root = allMonkeys["root"]!
     }
 
-    func part1() -> Decimal {
-        let root = monkeys["root"]!
-
-        return yell(of: root)
+    func part1() -> Int {
+        root.yell
     }
 
-    func part2() -> Decimal {
-        let root = monkeys["root"]!
-        guard case Yell.op(let var1, _, let var2) = root.yell else {
-            fatalError()
-        }
-
-        var map = [String: String]()
-        var check = "humn"
-        while check != "root" {
-            let prod = monkeys.values.compactMap { $0.uses(check) }
-            map[prod[0]] = check
-            check = prod[0]
-        }
-
-        let testMonkey = monkeys[map["root"]!]!
-        let searchMonkey = monkeys[var1 == testMonkey.name ? var2 : var1]!
-        let search = yell(of: searchMonkey, 0)
-
-        var lowerBound = 0
-        var upperBound = 1_000_000_000_000_000
-        var humn: Decimal?
-        while lowerBound < upperBound {
-            let index = lowerBound + (upperBound - lowerBound) / 2
-            let result = yell(of: testMonkey, Decimal(index))
-            if result == search {
-                humn = Decimal(index)
-                break
-            } else if result < search {
-                upperBound = index - 1
-            } else {
-                lowerBound = index + 1
-            }
-        }
-
-        return humn ?? 0
-    }
-
-    private func yell(of monkey: Monkey, _ humn: Decimal? = nil) -> Decimal {
-        switch monkey.yell {
-        case .constant(let int):
-            if let humn, monkey.name == "humn" {
-                return humn
-            }
-            return int
-        case .op(let var1, let op, let var2):
-            let v1 = yell(of: monkeys[var1]!, humn)
-            let v2 = yell(of: monkeys[var2]!, humn)
-            switch op {
-            case "+": return v1 + v2
-            case "-": return v1 - v2
-            case "*": return v1 * v2
-            case "/": return v1 / v2
-            default: fatalError()
-            }
-        }
+    func part2() -> Int {
+        let path = root.findHumanPath()
+        return root.calculateHumanValue(path: path, incoming: 0)
     }
 }
